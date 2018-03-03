dec8e3a467458e64c4647289fa92dcbebc9369d3
mzwae <mrmonjaie@gmail.com>
2018-03-03 22:24:48 +0000

    removed index.php from url

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -49,7 +49,9 @@ class News extends CI_Controller
         $this->load->view('templates/footer');
       } else {
         $this->news_model->set_news();
+        $this->load->view('templates/header', $data);
         $this->load->view('news/success');
+        $this->load->view('templates/footer');
       }
 
 
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -16,14 +16,14 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?=base_url()?>">News App</a>
+      <a class="navbar-brand" href="<?=base_url()?>news">News App</a>
   </div>
   <!-- Collection of nav links and other content for toggling -->
   <div id="navbarCollapse" class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
           <li class="nav-item"><a href="<?=base_url()?>news">Home</a></li>
           <li class="nav-item"><a href="<?=base_url()?>news">News</a></li>
-          <li class="nav-item"><a href="<?=base_url()?>/news/create">Create</a></li>
+          <li class="nav-item"><a href="<?=base_url()?>news/create">Create</a></li>
       </ul>
 
   </div>
